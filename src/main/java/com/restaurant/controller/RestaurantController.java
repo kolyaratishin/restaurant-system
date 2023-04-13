eff7fa4610bb2a4c667bf46b666f4f018143c708
kolyaratishin <kolyaratishin1@gmail.com>
2023-04-13 16:10:37 +0300

    added getAll endpoints for restaurant controller

--- src/main/java/com/restaurant/controller/RestaurantController.java
+++ src/main/java/com/restaurant/controller/RestaurantController.java
@@ -1,6 +1,9 @@
 package com.restaurant.controller;
 
 import com.restaurant.controller.dto.RestaurantDto;
+import com.restaurant.controller.dto.TableDto;
+import com.restaurant.controller.response.MealGroupResponse;
+import com.restaurant.controller.response.MealResponse;
 import com.restaurant.model.Restaurant;
 import com.restaurant.service.RestaurantService;
 import lombok.RequiredArgsConstructor;
@@ -29,7 +32,32 @@ public class RestaurantController {
     }
 
     @DeleteMapping("/{id}")
-    public void deleteById(@PathVariable(value = "id") Long id){
+    public void deleteById(@PathVariable(value = "id") Long id) {
         restaurantService.deleteById(id);
+    }
+
+    @GetMapping("/{restaurantId}/tables")
+    public List<TableDto> getAllTables(@PathVariable(value = "restaurantId") Long id) {
+        Restaurant restaurantById = restaurantService.getRestaurantById(id);
+        return restaurantById.getTables()
+                .stream()
+                .map(table -> modelMapper.map(table, TableDto.class))
+                .toList();
+    }
+
+    @GetMapping("/{restaurantId}/menu")
+    public List<MealGroupResponse> getMenu(@PathVariable(value = "restaurantId") Long id) {
+        Restaurant restaurantById = restaurantService.getRestaurantById(id);
+        return restaurantById.getMealGroups()
+                .stream()
+                .map(mealGroup -> {
+                    MealGroupResponse response = modelMapper.map(mealGroup, MealGroupResponse.class);
+                    response.setMenu(mealGroup.getMeals().stream()
+                            .map(meal -> modelMapper.map(meal, MealResponse.class))
+                            .toList());
+                    response.setRestaurantId(mealGroup.getRestaurant().getId());
+                    return response;
+                })
+                .toList();
     }
 }
